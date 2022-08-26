5144026aa00b8ea8ff17094028b1dbedab92cce8
Fan <aliciafzhang@gmail.com>
2022-08-25 18:08:28 -0700

    discover page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,9 +9,9 @@ import { IoMdAdd} from 'react-icons/io';
 import Logo from '../utils/tiktik-logo.png';
 const Navbar = () => {
   return (
-    <div>
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
-        <div>
+        <div className="w-[100px] md:w-[130px] md:h-[30px] h-[38px]">
           <Image
             className="cursor-pointer"
             src={Logo}
